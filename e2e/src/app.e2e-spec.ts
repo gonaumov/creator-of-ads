1a85d4d07c0a7e4459fed23769a9a8b5b4ba0d0a
Georgi Naumov <gonaumov@gmail.com>
2020-02-07 02:22:19 +0200

    Some improvements, fixes and generating deployables

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('creator-of-ads app is running!');
+    expect(page.getTitleText()).toEqual('Creator of ads app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'creator-of-ads'`, () => {
+  it(`should have as title 'Creator of ads'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('creator-of-ads');
+    expect(app.title).toEqual('Creator of ads');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('creator-of-ads app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Creator of ads app is running!');
   });
 });
